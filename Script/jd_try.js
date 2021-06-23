dc871e5d263b281ca39115bdb32e8e742c9ee882
hejun041 <601235760@qq.com>
2021-06-23 09:50:34 +0800

    Update jd_try.js

--- Script/jd_try.js
+++ Script/jd_try.js
@@ -9,7 +9,7 @@ update 2021/6/7
 ==========================Quantumultx=========================
 [task_local]
 # 取关京东店铺商品，请在 boxjs 修改取消关注店铺数量
-5 10 * * * https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_unsubscribe.js, tag=取关京东店铺商品, enabled=true
+30 12 * * * https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_unsubscribe.js, tag=取关京东店铺商品, enabled=true
 
 # 京东试用
 30 10 * * * https://raw.githubusercontent.com/ZCY01/daily_scripts/main/jd/jd_try.js, tag=京东试用, img-url=https://raw.githubusercontent.com/ZCY01/img/master/jdtryv1.png, enabled=true
