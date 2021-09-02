f5658c0211b8f662ca9aa82bc48d3d4d470d64f5
hejun041 <601235760@qq.com>
2021-09-02 18:29:16 +0800

    fix

--- Task/CheckUtils.js
+++ Task/CheckUtils.js
@@ -3,58 +3,58 @@ function checkStatus(context, name) {
   try {
     var $ = context, days = new Date().getDay();
     let readStatus = $.getdata('ReadStatus') || '{}';
-    let statusObj = { "isfinished": false, "day": 0, "running": false }
-    readStatus = JSON.parse(readStatus)
+    let statusObj = { "isfinished": false, "day": 0, "running": false };
+    readStatus = JSON.parse(readStatus);
     let obj = readStatus[name];
     if (!isNull(obj)) {
       statusObj = obj;
       if (statusObj.day == days && statusObj.isfinished && !CYCLE[name]) {
         $.msg("今天已经看完啦🎇~");
-        return false
+        return false;
       }
       if (statusObj.running) {
         $.msg("脚本正在运行中，本次退出🎇~");
-        return false
+        return false;
       }
     }
     statusObj.running = true;
     statusObj.day = days;
-    readStatus[name] = statusObj
+    readStatus[name] = statusObj;
     $.setdata(JSON.stringify(readStatus), 'ReadStatus');
-    return true
+    return true;
   } catch (error) {
-    console.log('checkStatus===>', error)
-    return false
+    console.log('checkStatus===>', error);
+    return false;
   }
 }
 
 function setStatus(context, name) {
   var $ = context, days = new Date().getDay();
-  let statusObj = {}, readStatus = {}
+  let statusObj = {}, readStatus = {};
   statusObj.running = false;
   statusObj.day = days;
   statusObj.isfinished = true;
   let obj = $.getdata('ReadStatus');
-  readStatus = JSON.parse(obj)
-  readStatus[name] = statusObj
+  readStatus = JSON.parse(obj);
+  readStatus[name] = statusObj;
   $.setdata(JSON.stringify(readStatus), 'ReadStatus');
 }
 
 function isNull(obj) {
   if (!obj) {
-    return true
+    return true;
   }
   if (typeof obj == 'object') {
     if (JSON.stringify(obj) == '{}' || obj.length == 0) {
-      return true
+      return true;
     }
   }
   if (typeof obj == 'string') {
     if (obj == '{}') {
-      return true
+      return true;
     }
   }
-  return false
+  return false;
 }
 
 //是否可循环执行
